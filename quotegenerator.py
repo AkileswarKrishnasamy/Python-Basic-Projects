3a6b389ad3685d8bbb2a065179e7eedec5babc0f
Akil <143276376+AkileswarKrishnasamy@users.noreply.github.com>
2023-08-28 22:45:47 +0530

    Add files via upload

---
+++ quotegenerator.py
@@ -0,0 +1,20 @@
+import requests
+import json
+import random
+import fontstyle
+
+response = requests.get("https://api.quotable.io/quotes")
+
+parsed_data = json.loads(response.text)
+
+
+choice = random.randint(1,len(parsed_data))
+
+quote = parsed_data["results"][choice]["content"] +"-"+parsed_data["results"][choice]["author"].upper()
+
+text = fontstyle.apply(quote,'bold/Italic/red')
+
+print(text)
+
+
+
